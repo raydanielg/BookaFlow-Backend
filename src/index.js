eff952be61b855b0f3a70462751e2e0ed2e622a4
raydanielg <airezra2+1@gmail.com>
2026-08-30 09:29:46 +0300

    Fix: increase express body parser limit to 10mb for image uploads

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@ app.use(
   })
 )
 app.use(morgan("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 app.use(cookieParser())
 
 // Serve uploaded files statically
